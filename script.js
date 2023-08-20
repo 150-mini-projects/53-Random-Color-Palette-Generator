83a0657a760dd839876b27e6a13ec4db49886579
interactivepix <122866945+interactivepix@users.noreply.github.com>
2023-08-20 05:25:30 +0100

    Commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const container = document.querySelector(".container");
+const generateBtn = document.querySelector(".generate-btn");
+
+const maxPaletteBoxes = 10;
+
+const generatePalette = () => {
+  container.innerHTML = "";
+  for (let i = 0; i < maxPaletteBoxes; i++) {
+    let randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
+    randomHex = `#${randomHex.padStart(6, "0")}`;
+
+    const color = document.createElement("li");
+    color.classList.add("color");
+    color.innerHTML = ` <div class="rect-box" style="background: ${randomHex}"></div>
+                             <span class="hex-value">${randomHex}</span>`;
+    color.addEventListener("click", () => copyColor(color, randomHex));
+    container.appendChild(color);
+  }
+};
+
+generatePalette();
+
+const copyColor = (elem, hexVal) => {
+  const colorElement = elem.querySelector(".hex-value");
+  navigator.clipboard
+    .writeText(hexVal)
+    .then(() => {
+      colorElement.innerText = "Copied!";
+      setTimeout(() => (colorElement.innerText = hexVal), 800);
+    })
+    .catch(() => alert("Failed to copy color"));
+};
+
+generateBtn.addEventListener("click", generatePalette);
